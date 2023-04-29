a65fc9340dd0feadf785f73b0a7f3f19ced68c33
Eliáš Bauer <zenbauhaus@gmail.com>
2023-04-29 14:46:55 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,94 @@
+import { useState, useEffect } from 'react'
+import { Canvas, extend, useLoader } from '@react-three/fiber'
+import { OrbitControls, Environment, useTexture, Effects, MeshDistortMaterial, Html } from '@react-three/drei'
+import { LUTPass, LUTCubeLoader } from 'three-stdlib'
+import { Color } from 'three'
+
+extend({ LUTPass })
+
+function Grading() {
+  const { texture3D } = useLoader(LUTCubeLoader, '/cubicle-99.CUBE')
+  return (
+    <Effects>
+      <lUTPass lut={texture3D} intensity={0.75} />
+    </Effects>
+  )
+}
+
+function Sphere({ setShowText, ...props }) {
+  const texture = useTexture('/terrazo.png')
+  const [scale, setScale] = useState(1)
+
+  const handleClick = () => {
+    window.open('https://lllzom.csb.app/', 'noopener noreferrer')
+  }
+
+  return (
+    <group>
+      <mesh
+        {...props}
+        scale={scale}
+        onClick={handleClick}
+        onPointerOver={() => {
+          setScale(1.2)
+          setShowText(true)
+        }}
+        onPointerOut={() => {
+          setScale(1)
+          setShowText(false)
+        }}>
+        <sphereGeometry args={[1, 64, 64]} />
+        <MeshDistortMaterial map={texture} clearcoat={1} clearcoatRoughness={0} roughness={0} metalness={0.5} />
+      </mesh>
+    </group>
+  )
+}
+
+export default function App() {
+  const [showText, setShowText] = useState(false)
+  const [fontLoaded, setFontLoaded] = useState(false)
+
+  useEffect(() => {
+    async function loadFont() {
+      try {
+        const font = new FontFace('Poppins-Black', "url('/Poppins-Black.woff')")
+        await font.load()
+        document.fonts.add(font)
+        setFontLoaded(true)
+      } catch (error) {
+        console.error('Error loading the font: \n', error)
+      }
+    }
+
+    loadFont()
+  }, [])
+
+  return (
+    <>
+      <Canvas frameloop="demand" camera={{ position: [0, 0, 5], fov: 45 }}>
+        <ambientLight />
+        <spotLight intensity={0.5} angle={0.2} penumbra={1} position={[5, 15, 10]} />
+        <Sphere setShowText={setShowText} />
+        <Grading />
+        <Environment preset="forest" background blur={1} />
+        <OrbitControls enableZoom={true} autoRotate={true} maxZoom={10} minZoom={10} />
+      </Canvas>
+      {fontLoaded && (
+        <div
+          style={{
+            display: showText ? 'block' : 'none',
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            fontSize: '3rem',
+            fontWeight: 'bold',
+            fontFamily: 'Poppins-Black',
+            pointerEvents: 'none',
+          }}>
+          V S T U P T E
+        </div>
+      )}
+    </>
+  )
+}
